e8350c5cfe79b3b9725de9d83812f1759e369431
PriyanSiva <priyansiva1995@gmail.com>
2021-03-13 07:47:12 +0530

    tasks, add ,delete functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,91 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <v-text-field
+      v-model="newTask"
+      class="ma-2"
+      outlined
+      label="Add  New Task"
+      append-icon="mdi-plus"
+      clearable
+      hide-details
+      @click:append="addTask"
+    ></v-text-field>
+
+    <div
+      v-for="task in tasks"
+      :key="task.id"
+    >
+
+        <v-list-item>
+
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          
+            <v-list-item-action>
+              <v-btn
+                icon
+                @click.stop="deleteTask(task.id)"
+              >
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+
+        </v-list-item>
+
+      <v-divider></v-divider>
+
+    </div>
+
   </div>
+    
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      newTask: '',
+      tasks: [
+        {
+          id: '1',
+          title: 'Wakeup',
+          done: false
+        },
+        {
+          id: '2',
+          title: 'Workout',
+          done: false
+        },
+        {
+          id: '3',
+          title: 'Study',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    addTask(){
+      let newTaskObj = {
+        id: Date.now(),
+        title: this.newTask,
+        done: false
+      }
+      this.tasks.unshift(newTaskObj)
+    } ,
+    deleteTask(taskId) {
+      this.tasks = this.tasks.filter(task => task.id != taskId)
+    }
   }
 }
 </script>
